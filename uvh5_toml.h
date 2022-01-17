5b01e69ac73bdf67986a0f1b3c4a56b7b5e356e6
RocketRoss <code@radonn.co.za>
2022-01-17 10:09:07 +0000

    * uvh5_toml.h match definition to declaration

--- uvh5_toml.h
+++ uvh5_toml.h
@@ -12,16 +12,16 @@ static inline int uvh5_toml_error(const char* msg, const char* msg1)
 		return 1;
 }
 
-int uvh5_toml_sexagesimal_in(toml_table_t* conf, const char* location, float* sexagesimal);
-int uvh5_toml_string_in(toml_table_t* conf, const char* location, char** string_out);
-int uvh5_toml_double_in(toml_table_t* conf, const char* location, double* double_out);
-int uvh5_toml_float_in(toml_table_t* conf, const char* location, float* float_out);
-int uvh5_toml_int_in(toml_table_t* conf, const char* location, int* int_out);
+int uvh5_toml_sexagesimal_in(toml_table_t* parent, const char* location, float* sexagesimal);
+int uvh5_toml_string_in(toml_table_t* parent, const char* location, char** string_out);
+int uvh5_toml_double_in(toml_table_t* parent, const char* location, double* double_out);
+int uvh5_toml_float_in(toml_table_t* parent, const char* location, float* float_out);
+int uvh5_toml_int_in(toml_table_t* parent, const char* location, int* int_out);
 
-int uvh5_toml_string_at(toml_array_t* array, const int idx, char** string_out);
-int uvh5_toml_double_at(toml_array_t* array, const int idx, double* double_out);
-int uvh5_toml_float_at(toml_array_t* array, const int idx, float* float_out);
-int uvh5_toml_int_at(toml_array_t* array, const int idx, int* int_out);
+int uvh5_toml_string_at(toml_array_t* parent, const int location, char** string_out);
+int uvh5_toml_double_at(toml_array_t* parent, const int location, double* double_out);
+int uvh5_toml_float_at(toml_array_t* parent, const int location, float* float_out);
+int uvh5_toml_int_at(toml_array_t* parent, const int location, int* int_out);
 
 int uvh5_toml_antenna_table_in(
 	toml_table_t* parent,
