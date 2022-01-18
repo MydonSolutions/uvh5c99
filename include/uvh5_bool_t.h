2f79f00a29691962037bf50c8cc916559e6f3537
RocketRoss <code@radonn.co.za>
2022-01-18 13:04:48 +0200

    * uvh5_bool_t.h ->> uvh5_bool_t.c/h

--- include/uvh5_bool_t.h
+++ include/uvh5_bool_t.h
@@ -9,12 +9,6 @@ typedef enum {
 } UVH5_bool_t;
 
 #define CPTR(VAR,CONST) ((VAR)=(CONST),&(VAR))
-static hid_t UVH5TcreateBool() {
-	hid_t UVH5_bool = H5Tcreate(H5T_ENUM, sizeof(UVH5_bool_t));
-	UVH5_bool_t val;
-	H5Tenum_insert(UVH5_bool, "FALSE", CPTR(val, UVH5_FALSE));
-	H5Tenum_insert(UVH5_bool, "TRUE" , CPTR(val, UVH5_TRUE));
-	return UVH5_bool;
-}
+hid_t UVH5TcreateBool();
 
 #endif
